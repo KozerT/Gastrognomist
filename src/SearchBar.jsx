f157a6ebdda97f125cf6ab0c25ef95c5fe3784a4
KozerT <kozer.vizastaff@gmail.com>
2023-12-11 12:45:05 +0100

    feat: Prepare search functionality structure (WIP)

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -2,9 +2,23 @@ import React from "react";
 
 const SearchBar = () => {
   return (
-    <div className="flex items-center flex-wrap justify-center flex-col gap-y-7  py-36">
-      <div></div>
-      <hr className="border-b border-dashed border-blue-600" />
+    <div className="flex items-center flex-wrap justify-center flex-col gap-y-7 py-36 ">
+      <div className="flex flex-row gap-x-32 px-4">
+        <span className="block w-full text-center ">
+          Best
+          <br /> Match
+        </span>
+        <span className="block w-full text-center">
+          Highest
+          <br /> Rated
+        </span>
+        <span className="block w-full  text-center">
+          Most <br />
+          Reviewed
+        </span>
+      </div>
+      <hr className="w-[80%] border-1 border-black" />
+
       <div className="flex  bg-white rounded-md  shadow-slate-200 w-8/12 gap-36 justify-around">
         <input
           type="text"
