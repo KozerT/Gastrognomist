6bec147aec0e30543a0bb4c72be22630efa3d517
KozerT <kozer.vizastaff@gmail.com>
2023-12-11 12:07:56 +0100

    feat: Implemeneted  SearchBar component for user input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import BusinessList from "./BusinessList";
+import SearchBar from "./SearchBar";
 
 function App(props) {
   return (
     <div className="App">
+      <SearchBar />
       <BusinessList />
     </div>
   );
---
+++ src/SearchBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div className="flex items-center flex-wrap justify-center flex-col gap-y-7  py-36">
+      <div></div>
+      <hr className="border-b border-dashed border-blue-600" />
+      <div className="flex  bg-white rounded-md  shadow-slate-200 w-8/12 gap-36 justify-around">
+        <input
+          type="text"
+          className="block w-full px-4 py-2 text-slate-950 bg-white border rounded-md  focus:outline-none  "
+          placeholder="Search Business"
+        />
+        <input
+          type="text"
+          className="block w-full px-4 py-2  text-slate-950 bg-white border rounded-md  focus:outline-none "
+          placeholder="Where?"
+        />
+      </div>
+      <div>
+        <button className="px-6 py-2 text-white font-semibold bg-yellow-500  border-l rounded ">
+          Let's go
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
