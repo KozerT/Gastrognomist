70feb183f5d4e0b482e330f8d7be832a82de1413
KozerT <kozer.vizastaff@gmail.com>
2023-12-16 14:33:18 +0100

    refactor: Improved the structure of the code by moving the array of businesses from the App component to the BusinessList component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,39 @@ import SearchBar from "./components/SearchBar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-function App(props) {
+const testData = [
+  {
+    imageSrc:
+      "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+    name: "MarginOtto Pizzeria",
+    address: "1010 Paddington Way",
+    city: "Flavortown",
+    state: "NY",
+    zipCode: "10101",
+    category: "Italian",
+    rating: 4.5,
+    reviewCount: 90,
+  },
+  {
+    imageSrc:
+      "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+    name: " Kebab-pizza",
+    address: "1000 Solna Stadd",
+    city: "Solna",
+    state: "Stockholm",
+    zipCode: "17288",
+    category: "Swedish",
+    rating: 4.7,
+    reviewCount: 61,
+  },
+];
+
+function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={testData} />
       <Footer />
     </div>
   );
--- src/components/BusinessList.jsx
+++ src/components/BusinessList.jsx
@@ -1,37 +1,10 @@
 import React from "react";
 import Business from "./Business";
 
-const testData = [
-  {
-    imageSrc:
-      "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
-    name: "MarginOtto Pizzeria",
-    address: "1010 Paddington Way",
-    city: "Flavortown",
-    state: "NY",
-    zipCode: "10101",
-    category: "Italian",
-    rating: 4.5,
-    reviewCount: 90,
-  },
-  {
-    imageSrc:
-      "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
-    name: " Kebab-pizza",
-    address: "1000 Solna Stadd",
-    city: "Solna",
-    state: "Stockholm",
-    zipCode: "17288",
-    category: "Swedish",
-    rating: 4.7,
-    reviewCount: 61,
-  },
-];
-
-const BusinessList = () => {
+const BusinessList = ({ businesses }) => {
   return (
     <section className="flex flex-wrap justify-center gap-24">
-      {testData.map((business, index) => (
+      {businesses.map((business, index) => (
         <Business key={index} {...business} />
       ))}
     </section>
