968a92a9f8d25d65ddc2cb42d09b421041d53f0c
KozerT <kozer.vizastaff@gmail.com>
2023-12-28 11:58:41 +0100

    refactor: Consolidate API credentials in .env

--- src/utils/yelpAPI.js
+++ src/utils/yelpAPI.js
@@ -1,7 +1,4 @@
-const API_KEY =
-  "O2V8fnGI2CwpXdhKHUrondcaz2PTZaa3rYkaxrCf1l8v6676_t2POsZJ5YNQVstsGfKQMQ3l_wZFQ_O1tUk8LebVcJhbL5uvneN-xf-L9DiF-hcM6i8cxtscLxuMZXYx";
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
-const API_URL = "https://api.yelp.com/v3/businesses/search";
 
 export const searchBusinesses = async (searchTerm, location, sortBy) => {
   try {
@@ -9,12 +6,12 @@ export const searchBusinesses = async (searchTerm, location, sortBy) => {
       method: "GET",
       headers: {
         accept: "application/json",
-        Authorization: `Bearer ${API_KEY}`, // Replace with your actual Yelp API key
+        Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
       },
     };
 
     const response = await fetch(
-      `${CORS_PROXY}${API_URL}?term=${searchTerm}&location=${location}&sort_by=${sortBy}`,
+      `${CORS_PROXY}${process.env.REACT_APP_API_YELP_URL}?term=${searchTerm}&location=${location}&sort_by=${sortBy}`,
       {
         method: "GET",
         headers: {
