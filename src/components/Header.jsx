8e9a3b90c0f545f1e43b0eff16be23eea93cc43d
KozerT <kozer.vizastaff@gmail.com>
2023-12-30 22:23:47 +0100

    refactor: Optimize file structure and remove unused files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="flex flex-wrap items-center justify-center py-2 text-2xl font-semibold text-white bg-yellow-500 ">
-      ravenous
+      Gastrognomist
     </div>
   );
 };
