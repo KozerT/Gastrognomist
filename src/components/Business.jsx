d6850df70986c8f7fced6c67e6d995b40f446314
KozerT <kozer.vizastaff@gmail.com>
2024-05-06 14:46:44 +0200

    refacto: include yelp properties to display data in the Business component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 
-const Business = (props) => {
+const Business = ({business}) => {
+
+
   return (
     <div className="w-3/12 overflow-hidden bg-white rounded-md shadow-md shadow-slate-200 ">
       <img
-        src={props.business.imageSrc}
+        src={business.image_url}
         alt=""
         className="w-full object-cover max-h-[22rem]"
       />
       <div className="w-full p-6 ">
         <h3 className="mb-3 text-xl font-semibold text-black text-start">
-          {props.business.name}
+          {business.name}
         </h3>
         <div className="inline-flex w-full ">
           <address className="flex flex-col w-1/2 gap-2 not-italic text-left ">
-            <p>{props.business.address}</p>
-            <p>{props.business.city}</p>
-            <p>{`${props.business.state} ${props.business.zipCode}`}</p>
+            <p>{business.location.address1}</p>
+            <p>{business.location.city}</p>
+            <p>{`${business.location.state} ${business.location.zip_code}`}</p>
           </address>
           <div className="flex flex-col w-1/2 gap-2 text-right ">
-            <p className="font-medium text-yellow-600 uppercase ">
-              {props.business.category}
-            </p>
+          {business.categories.map((category) => (
+              <p key={category.alias} className="font-medium text-yellow-600 uppercase">
+                {category.title}
+              </p>
+            ))}
             <p className="font-medium text-yellow-600 ">
-              {props.business.rating}
+              {business.rating}
             </p>
-            <p>{props.business.reviewCount} reviews </p>
+            <p>{business.review_count} reviews </p>
           </div>
         </div>
       </div>
